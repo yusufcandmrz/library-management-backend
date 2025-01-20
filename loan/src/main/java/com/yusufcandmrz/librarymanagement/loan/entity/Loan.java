4fc8dee11110c6c4c2f1162296a21f9080ed66c7
Yusuf Demiroz 84396111 <yusuf.demiroz1@h-partners.com>
2025-01-20 16:24:29 +0300

    loan updated

--- loan/src/main/java/com/yusufcandmrz/librarymanagement/loan/entity/Loan.java
+++ loan/src/main/java/com/yusufcandmrz/librarymanagement/loan/entity/Loan.java
@@ -1,14 +1,40 @@
 package com.yusufcandmrz.librarymanagement.loan.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.UUID;
 
+@Entity
+@Table(name = "loan")
 public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
-    private Book book;
-    private Account account;
-    private String status; //(Enum: 'LOANED', 'RETURNED', 'OVERDUE');
+
+    @Column(name = "account_id", nullable = false)
+    private UUID accountId;
+
+    @ElementCollection
+    @CollectionTable(name = "loan_book", joinColumns = @JoinColumn(name = "loan_id"))
+    @Column(name = "book_id", nullable = false)
+    private List<UUID> bookIds;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private LoanStatus status;
+
+    @CreationTimestamp
+    @Column(name = "loan_date", updatable = false, nullable = false)
     private Timestamp loanDate;
+
+    @Column(name = "due_date")
     private Timestamp dueDate;
+
+    @Column(name = "return_date")
     private Timestamp returnDate;
 }
