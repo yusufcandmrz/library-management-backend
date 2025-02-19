f15e64bc5cb43d33fbbc944829542fe16bb6a484
Yusuf Demiroz 84396111 <yusufcandmrz@gmail.com>
2025-02-19 16:27:52 +0300

    auth updated

---
+++ auth/src/main/java/com/yusufcandmrz/librarymanagement/auth/entity/Auth.java
@@ -0,0 +1,35 @@
+package com.yusufcandmrz.librarymanagement.auth.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "auth")
+public class Auth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+}
---
+++ auth/src/main/java/com/yusufcandmrz/librarymanagement/auth/entity/Status.java
@@ -0,0 +1,6 @@
+package com.yusufcandmrz.librarymanagement.auth.entity;
+
+public enum Status {
+    ACTIVE,
+    INACTIVE
+}
