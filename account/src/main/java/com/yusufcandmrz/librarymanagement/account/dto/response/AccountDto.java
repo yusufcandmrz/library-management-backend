e8055fbb103d549de8a717c5eddc8b4c9ae37708
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-06-29 11:23:42 +0300

    Updated AccountDto in account service for modelmapper usage

--- account/src/main/java/com/yusufcandmrz/librarymanagement/account/dto/response/AccountDto.java
+++ account/src/main/java/com/yusufcandmrz/librarymanagement/account/dto/response/AccountDto.java
@@ -1,10 +1,12 @@
 package com.yusufcandmrz.librarymanagement.account.dto.response;
 
 import com.yusufcandmrz.librarymanagement.account.entity.Status;
+import lombok.Data;
 
 import java.util.UUID;
 import java.sql.Timestamp;
 
+@Data
 public class AccountDto {
     UUID id;
     String firstName;
