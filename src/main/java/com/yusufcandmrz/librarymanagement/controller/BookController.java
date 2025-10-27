b83e616cee29af024a70448a72d42f1eea3f6800
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-10-27 23:23:43 +0300

    Create controller classes

---
+++ src/main/java/com/yusufcandmrz/librarymanagement/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.yusufcandmrz.librarymanagement.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+
+    //TODO Add Book: POST -> /books
+    //TODO List Books: GET -> /books
+    //TODO Get Book by ID: GET -> /books/{id}
+    //TODO Update Book: POST -> /books/{id}
+    //TODO Delete Book: DELETE -> /books/{id}
+}
---
+++ src/main/java/com/yusufcandmrz/librarymanagement/controller/ReaderController.java
@@ -0,0 +1,13 @@
+package com.yusufcandmrz.librarymanagement.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReaderController {
+
+    //TODO Add Reader: POST -> /readers
+    //TODO List Readers: GET -> /readers
+    //TODO Borrow Book: POST -> /readers/{readerId}/borrow/{bookId} marks book unavailable
+    //TODO Return Book: POST -> /readers/{readerId}/return/{bookId} marks available again
+
+}
