570aaf6765921f995bda95e31e6d88ac257f828a
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-10-29 22:58:02 +0300

    Update BookController

--- src/main/java/com/yusufcandmrz/librarymanagement/controller/BookController.java
+++ src/main/java/com/yusufcandmrz/librarymanagement/controller/BookController.java
@@ -1,9 +1,18 @@
 package com.yusufcandmrz.librarymanagement.controller;
 
+import com.yusufcandmrz.librarymanagement.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookController {
+
+    BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     //TODO Add Book: POST -> /books
     //TODO List Books: GET -> /books
