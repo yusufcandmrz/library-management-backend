ea6098b45a64daf06f1aa1887ab3fd7d52fa2b22
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-10-31 22:13:32 +0300

    Update BookController

--- src/main/java/com/yusufcandmrz/librarymanagement/controller/BookController.java
+++ src/main/java/com/yusufcandmrz/librarymanagement/controller/BookController.java
@@ -2,9 +2,14 @@ package com.yusufcandmrz.librarymanagement.controller;
 
 import com.yusufcandmrz.librarymanagement.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(name = "/api")
 public class BookController {
 
     BookService bookService;
@@ -12,6 +17,11 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @PostMapping(name = "/books")
+    public ResponseEntity<String> addBook() {
+        return new ResponseEntity<>("Book added",HttpStatus.OK);
     }
 
     //TODO Add Book: POST -> /books
