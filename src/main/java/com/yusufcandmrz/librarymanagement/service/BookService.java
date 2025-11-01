1c272d9ad1bf7dea442c063ba1c50ce43bdf852a
yusufcandmrz <93606208+yusufcandmrz@users.noreply.github.com>
2025-11-01 20:45:07 +0300

    Update BookService

--- src/main/java/com/yusufcandmrz/librarymanagement/service/BookService.java
+++ src/main/java/com/yusufcandmrz/librarymanagement/service/BookService.java
@@ -1,7 +1,22 @@
 package com.yusufcandmrz.librarymanagement.service;
 
+import com.yusufcandmrz.librarymanagement.dto.AddBookRequest;
+import com.yusufcandmrz.librarymanagement.dto.UpdateBookRequest;
+import com.yusufcandmrz.librarymanagement.entity.Book;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface BookService {
+
+    public void addBook(AddBookRequest request);
+
+    public List<Book> getBookList();
+
+    public Book getBookById(Long id);
+
+    public void updateBookById(Long id, UpdateBookRequest request);
+
+    public void deleteBookById(Long id);
 }
--- src/main/java/com/yusufcandmrz/librarymanagement/service/imp/BookServiceImp.java
+++ src/main/java/com/yusufcandmrz/librarymanagement/service/imp/BookServiceImp.java
@@ -1,6 +1,35 @@
 package com.yusufcandmrz.librarymanagement.service.imp;
 
+import com.yusufcandmrz.librarymanagement.dto.AddBookRequest;
+import com.yusufcandmrz.librarymanagement.dto.UpdateBookRequest;
+import com.yusufcandmrz.librarymanagement.entity.Book;
 import com.yusufcandmrz.librarymanagement.service.BookService;
 
+import java.util.List;
+
 public class BookServiceImp implements BookService {
+    @Override
+    public void addBook(AddBookRequest request) {
+
+    }
+
+    @Override
+    public List<Book> getBookList() {
+        return List.of();
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void updateBookById(Long id, UpdateBookRequest request) {
+
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+
+    }
 }
